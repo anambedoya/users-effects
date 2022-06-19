62ba26cdffc86e5ba87727f3895adc6e0ee1e30c
Ana Maria Bedoya <anabedoya1997@gmail.com>
2022-06-19 11:23:16 -0500

    Effects, error handling for effects and show info for users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { appReducers } from './store/app.reducer';
 
@@ -15,6 +16,7 @@ import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { EffectsArray } from './store/effects';
 
 @NgModule({
   declarations: [
@@ -27,6 +29,7 @@ import { UsuariosModule } from './usuarios/usuarios.module';
     SharedModule,
     UsuariosModule,
     StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot(EffectsArray),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
